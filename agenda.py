50b184df2a70bb70f9055315117057e9d29168bb
Aschull <andrews.fernandesx@gmail.com>
2022-06-22 11:38:07 -0300

    Finalizacao do desenvolvimento da agenda.

--- agenda.py
+++ agenda.py
@@ -1,12 +1,24 @@
 import os
 
-from app.entities.agenda import Agenda
-
 
 def clear(): return os.system('cls')
 
 
 contatos = []
+
+
+class Agenda(object):
+    def __init__(self, id, nome, telefone, cidade, estado, status):
+        self.id = id
+        self.nome = nome
+        self.telefone = telefone
+        self.cidade = cidade
+        self.estado = estado
+        self.status = status
+
+    def __repr__(self):
+        return 'Contato: <ID: {}, Nome: {}, Telefone: {}, Cidade: {}, Estado: {}, Status: {}>'.format(
+            self.id, self.nome, self.telefone, self.cidade, self.estado, self.status)
 
 
 def create_menu():
